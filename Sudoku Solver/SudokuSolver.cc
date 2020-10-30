9f34582ba11cc8a3335903b0dd4a64b4472dcf3a
abhinavrawat882 <54599582+abhinavrawat882@users.noreply.github.com>
2020-10-30 09:26:56 +0530

    basic layout

--- Sudoku Solver/SudokuSolver.cc
+++ Sudoku Solver/SudokuSolver.cc
@@ -1,10 +1,88 @@
 #include<stdio.h>
+int su[9][9];// stores main sudoku
+int rr[9][9];//rr means rows record
+int cr[9][9];//cr means column record
+int br[9][9];//br is the block record
+int FindBlock(int i,int y){
+    if(i>=0&&i<=2){
+        if(y>=0&&y<=2){
+            return 0;
+        }
+        else if(y>2&&y<=5){
+            return 1;
 
+        }   
+        else{
+            return 2;
+        
+        } 
+
+    }
+    else if(i>2&&i<=5){
+        if(y>=0&&y<=2){
+            return 3;
+        }
+        else if(y>2&&y<=5){
+            return 4;
+
+        }   
+        else{
+            return 5;
+        
+        } 
+    }   
+    else{
+        if(y>=0&&y<=2){
+            return 6;
+        }
+        else if(y>2&&y<=5){
+            return 7;
+
+        }   
+        else{
+            return 8;
+        
+        } 
+    } 
+}
+void PrintSudoku(int* sd){
+    for(int i=0;i<9;i++){
+        for(int y=0;y<9;y++){
+            printf("%d ",sd[i][y]);
+        }
+    }
+
+}
+void FindNextEmpty(int x,int y,int* xx,int* yy){
+    for(;y<9;y++){
+        if(su[x][y]==0){
+            *xx=x;
+            *yy=y;
+            return;
+        }
+    }
+    for(x++;x<9;x++){
+        for(y=0;y<9;y++){
+            if(su[x][y]==0){
+            *xx=x;
+            *yy=y;
+            return;
+        }
+        }
+    }
+    *xx=-1;
+            *yy=-1;
+            return;
+
+
+
+}
+void recursion(x,y){
+    
+}
 int main(){
-    int su[9][9];// stores main sudoku
-    int rr[9][9];//rr means rows record
-    int cr[9][9];//cr means column record
-    int br[9][9];//br is the block record
+   
+    
     for(int i=0;i<9;i++){
         for(int y=0;y<9;y++){
             rr[i][y]=0;
