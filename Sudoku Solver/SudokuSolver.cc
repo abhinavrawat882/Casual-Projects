e40f74f5e0b304e345115f3417e4937b8526527b
abhinavrawat882 <54599582+abhinavrawat882@users.noreply.github.com>
2020-10-30 20:08:54 +0530

    sudoku solver complete

--- Sudoku Solver/SudokuSolver.cc
+++ Sudoku Solver/SudokuSolver.cc
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int su[9][9];// stores main sudoku
-int rr[9][9];//rr means rows record
-int cr[9][9];//cr means column record
-int br[9][9];//br is the block record
+int rr[9][10];//rr means rows record
+int cr[9][10];//cr means column record
+int br[9][10];//br is the block record
 int FindBlock(int i,int y){
     if(i>=0&&i<=2){
         if(y>=0&&y<=2){
@@ -92,16 +92,18 @@ void unupdate(int x,int y,int i){
 }
 int recursion(int x,int y){
    //PrintSudoku();
-    for(int i=1;i<9;i++){
+    for(int i=1;i<=9;i++){
         if(rr[x][i]==0&&cr[y][i]==0&&br[FindBlock(x,y)][i]==0){
             
             update(x,y,i);
             int a,b;
             int c=FindNextEmpty(x,y,&a,&b);
-            printf("%d %d\n",a,b);
-            printf("%d\n",FindBlock(x,y));
+           // printf("%d %d\n",a,b);
+           // printf("%d\n",FindBlock(x,y));
             
-            PrintSudoku();
+            //PrintSudoku();
+            //int stopvr;
+            //scanf("%d",&stopvr);
             if(c==1){
                 
                 int xn=recursion(a,b);
@@ -128,8 +130,8 @@ int recursion(int x,int y){
 int main(){
    
     
-    for(int i=0;i<9;i++){
-        for(int y=0;y<9;y++){
+    for(int i=0;i<=9;i++){
+        for(int y=0;y<=9;y++){
             rr[i][y]=0;
             cr[i][y]=0;
             br[i][y]=0;
