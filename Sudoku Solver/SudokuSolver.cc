6ce58c8592e9fee5b7cad0fcb313d0628d57323d
abhinavrawat882 <54599582+abhinavrawat882@users.noreply.github.com>
2020-10-30 19:38:19 +0530

    added recursion fun

--- Sudoku Solver/SudokuSolver.cc
+++ Sudoku Solver/SudokuSolver.cc
@@ -45,20 +45,21 @@ int FindBlock(int i,int y){
         } 
     } 
 }
-void PrintSudoku(int* sd){
+void PrintSudoku(){
     for(int i=0;i<9;i++){
         for(int y=0;y<9;y++){
-            printf("%d ",sd[i][y]);
+            printf("%d ",su[i][y]);
         }
+        printf("\n");
     }
 
 }
-void FindNextEmpty(int x,int y,int* xx,int* yy){
+int FindNextEmpty(int x,int y,int* xx,int* yy){
     for(;y<9;y++){
         if(su[x][y]==0){
             *xx=x;
             *yy=y;
-            return;
+            return 1;
         }
     }
     for(x++;x<9;x++){
@@ -66,18 +67,62 @@ void FindNextEmpty(int x,int y,int* xx,int* yy){
             if(su[x][y]==0){
             *xx=x;
             *yy=y;
-            return;
+            return 1;
         }
         }
     }
-    *xx=-1;
-            *yy=-1;
-            return;
+   
+            return -1;
 
 
 
 }
-void recursion(x,y){
+void update(int x,int y,int i){
+    rr[x][i]=1;
+    cr[y][i]=1;
+    //PrintSudoku();
+    br[FindBlock(x,y)][i]=1;
+    su[x][y]=i;
+}
+void unupdate(int x,int y,int i){
+    rr[x][i]=0;
+    cr[y][i]=0;
+    su[x][y]=0;
+    br[FindBlock(x,y)][i]=0;
+}
+int recursion(int x,int y){
+   //PrintSudoku();
+    for(int i=1;i<9;i++){
+        if(rr[x][i]==0&&cr[y][i]==0&&br[FindBlock(x,y)][i]==0){
+            
+            update(x,y,i);
+            int a,b;
+            int c=FindNextEmpty(x,y,&a,&b);
+            printf("%d %d\n",a,b);
+            printf("%d\n",FindBlock(x,y));
+            
+            PrintSudoku();
+            if(c==1){
+                
+                int xn=recursion(a,b);
+                if(xn==-1){
+
+                    unupdate(x,y,i);
+                    continue;
+                }
+                else{
+                    return 1;
+                }
+
+            }
+            else{
+                return (1);
+            }
+
+
+        }
+    }return -1;
+
     
 }
 int main(){
@@ -105,6 +150,6 @@ int main(){
     FindNextEmpty(0,0,&x,&y);//this finds the next empty place in sudoku 
     
     recursion(x,y);//this starts the recursion method to recursivly put values in each empty box and brute force the solution of sudoku
-
+    PrintSudoku();
     return 0;
 }
